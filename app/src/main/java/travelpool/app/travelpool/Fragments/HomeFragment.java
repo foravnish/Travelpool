bf98abdbaf66d6e7d47ce43c1e1a823f7e5c79c6
Danny <dinesh.kumar@sntinfotech.com>
2018-06-28 13:46:31 +0530

    update

--- app/src/main/java/travelpool/app/travelpool/Fragments/HomeFragment.java
+++ app/src/main/java/travelpool/app/travelpool/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package travelpool.app.travelpool.Fragments;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -7,14 +8,17 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.adefruandta.spinningwheel.SpinningWheelView;
 
@@ -30,6 +34,7 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
     SpinningWheelView wheelView;
+    Dialog dialog, dialog4;
 
     String[] value={"Abc","Xyx","ijk","lmn","poi","uud","jis","yys","usu","puf","etf"};
     @Override
@@ -77,6 +82,56 @@ public class HomeFragment extends Fragment {
                 wheelView.rotate(50, 5000, 50);
             }
         });
+
+
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+
+                        Button Yes_action,No_action;
+                        TextView heading;
+                        dialog4 = new Dialog(getActivity());
+                        dialog4.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                        dialog4.setContentView(R.layout.update_state1);
+
+                        Yes_action=(Button)dialog4.findViewById(R.id.Yes_action);
+                        No_action=(Button)dialog4.findViewById(R.id.No_action);
+                        heading=(TextView)dialog4.findViewById(R.id.heading);
+
+
+                        heading.setText("Are you sure you want to exit?");
+                        Yes_action.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                //System.exit(0);
+                                //getActivity().finish();
+                                getActivity().finishAffinity();
+
+                            }
+                        });
+
+                        No_action.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog4.dismiss();
+                            }
+                        });
+                        dialog4.show();
+//
+
+                        //Toast.makeText(getActivity(), "back", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
         return view;
 
     }
